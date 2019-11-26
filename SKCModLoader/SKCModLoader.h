58a0045d5f8bf33b10e059120f6c01772793bc3d
MainMemory <mainmemory@yahoo.com>
2019-11-25 20:49:04 -0600

    Inlining stuff from Utils.hpp.

--- SKCModLoader/SKCModLoader.h
+++ SKCModLoader/SKCModLoader.h
@@ -7,12 +7,58 @@
 
 #include <stdint.h>
 
-#include "Utils.hpp"
-
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
 #include <windows.h>
+
+#ifdef __cplusplus
+// C++ version.
+
+/**
+* Get the number of elements in an array.
+* @return Number of elements in the array.
+*/
+template <typename T, size_t N>
+static inline size_t LengthOfArray(const T(&)[N])
+{
+	return N;
+}
+
+/**
+* Get the size of an array.
+* @return Size of the array, in bytes.
+*/
+template <typename T, size_t N>
+static inline size_t SizeOfArray(const T(&)[N])
+{
+	return N * sizeof(T);
+}
+#else
+
+// C version.
+
+/**
+* Number of elements in an array.
+*
+* Includes a static check for pointers to make sure
+* a dynamically-allocated array wasn't specified.
+* Reference: http://stackoverflow.com/questions/8018843/macro-definition-array-size
+*/
+#define LengthOfArray(x) \
+	((int)(((sizeof(x) / sizeof(x[0]))) / \
+		(size_t)(!(sizeof(x) % sizeof(x[0])))))
+
+#define SizeOfArray(x) sizeof(x)
+
+#endif
+
+// Macros for functions that need both an array
+// and the array length or size.
+#define arrayptrandlength(data) data, LengthOfArray(data)
+#define arraylengthandptr(data) LengthOfArray(data), data
+#define arrayptrandsize(data) data, SizeOfArray(data)
+#define arraysizeandptr(data) SizeOfArray(data), data
 
 static inline BOOL WriteData(void *writeaddress, const void *data, SIZE_T datasize, SIZE_T *byteswritten)
 {
